8ae927faba5ccf6d8a735097c9ccca0400ce2ab2
thegrreat1 <thegrreat1@gmail.com>
2023-05-09 15:05:17 -0400

    Fixed code

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,18 +6,13 @@ export ARCH=arm64
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 
 make exynos9830-z3sxxx_defconfig
-make -j16
+make -j$(nproc --all)
 
 wait
 
-FILE=/home/kali/kernel/arch/arm64/boot/Image
+FILE=arch/arm64/boot/Image
 if test -f "$FILE"; then 
     cp "$FILE" Image
     wait
-    zip /media/sf_anykernels/Nethunter-s20ultra.zip Image
-    wait
-    echo ""
-    echo "Uploading Nethunter-s20ultra to tempsh"
-    curl -T /media/sf_anykernels/Nethunter-s20ultra.zip temp.sh
-    wait
+    zip arch/arm64/boot//Nethunter-s20ultra.zip Image
 fi
