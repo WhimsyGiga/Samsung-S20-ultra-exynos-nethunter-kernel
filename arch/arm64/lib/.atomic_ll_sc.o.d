f6918a737da13c0ec02fd412b94c254b2b3ff8fd
thegrreat1 <thegrreat1@gmail.com>
2023-05-11 10:27:23 -0400

    Fixed some issues

---
+++ arch/arm64/lib/.atomic_ll_sc.o.d
@@ -0,0 +1,21 @@
+atomic_ll_sc.o: arch/arm64/lib/atomic_ll_sc.c include/linux/kconfig.h \
+  include/generated/autoconf.h include/linux/compiler_types.h \
+  include/linux/compiler-clang.h arch/arm64/include/asm/atomic.h \
+  include/linux/compiler.h include/uapi/linux/types.h \
+  arch/arm64/include/generated/uapi/asm/types.h \
+  include/uapi/asm-generic/types.h include/asm-generic/int-ll64.h \
+  include/uapi/asm-generic/int-ll64.h \
+  arch/arm64/include/uapi/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/uapi/asm-generic/bitsperlong.h \
+  include/uapi/linux/posix_types.h include/linux/stddef.h \
+  include/uapi/linux/stddef.h arch/arm64/include/uapi/asm/posix_types.h \
+  include/uapi/asm-generic/posix_types.h \
+  arch/arm64/include/asm/barrier.h include/asm-generic/barrier.h \
+  include/linux/kasan-checks.h include/linux/types.h \
+  arch/arm64/include/asm/lse.h include/linux/export.h \
+  include/linux/stringify.h arch/arm64/include/asm/alternative.h \
+  arch/arm64/include/asm/cpucaps.h arch/arm64/include/asm/insn.h \
+  include/linux/init.h arch/arm64/include/asm/atomic_lse.h \
+  arch/arm64/include/asm/cmpxchg.h include/linux/build_bug.h \
+  arch/arm64/include/asm/atomic_ll_sc.h
